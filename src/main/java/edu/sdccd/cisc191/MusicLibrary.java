387d7fc978d6785e659cc8ef691ccaebbd0ce510
mahyars <mahyar.saadati@gmail.com>
2023-03-07 16:32:22 -0800

    This one is working, after changing the MusicLibrary.java to have 2D-Array

--- src/main/java/edu/sdccd/cisc191/MusicLibrary.java
+++ src/main/java/edu/sdccd/cisc191/MusicLibrary.java
@@ -1,44 +1,125 @@
 package edu.sdccd.cisc191;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MusicLibrary {
-    // the list of music tracks in the library
-    private List<MusicTrack> tracks;
+    // 2D array to store tracks by genre and album
+    private MusicTrack[][] tracks;
 
-    // initialize the list of music tracks to an empty ArrayList
     public MusicLibrary() {
-        tracks = new ArrayList<>();
+        // Initialize the 2D array with 10 genres and 10 albums per genre
+        tracks = new MusicTrack[10][10];
     }
 
-    // Add a music track to the library
     public void addTrack(MusicTrack track) {
-        // initialize the list of music tracks to an empty ArrayList
-        tracks.add(track);
+        // Add the track to the 2D array
+        String genre = track.getGenre();
+        int genreIndex = getGenreIndex(genre);
+
+        if (genreIndex == -1) {
+            // The genre does not exist in the 2D array, so add a new row for it
+            int newRow = tracks.length;
+            MusicTrack[] newGenreArray = {new MusicTrack(genre, "", "", "", "")};
+            tracks = Arrays.copyOf(tracks, newRow + 1);
+            tracks[newRow] = newGenreArray;
+            genreIndex = newRow;
+        }
+        // Add the track to the 2D array
+        int newLength = tracks[genreIndex].length + 1;
+        tracks[genreIndex] = Arrays.copyOf(tracks[genreIndex], newLength);
+        tracks[genreIndex][newLength - 1] = track;
     }
 
-    // Remove a music track from the library
     public void removeTrack(MusicTrack track) {
-        // remove the music track from the list of music tracks
-        tracks.remove(track);
+        // Remove the track from the 2D array
+        String genre = track.getGenre();
+        int genreIndex = getGenreIndex(genre);
+        if (genreIndex != -1) {
+            for (int i = 1; i < tracks[genreIndex].length; i++) {
+                if (tracks[genreIndex][i].equals(track)) {
+                    int lastColumn = tracks[genreIndex].length - 1;
+                    MusicTrack[] newTrackArray = new MusicTrack[lastColumn];
+                    for (int j = 1, k = 0; j < tracks[genreIndex].length; j++, k++) {
+                        if (j == i) {
+                            k--;
+                        } else {
+                            newTrackArray[k] = tracks[genreIndex][j];
+                        }
+                    }
+                    tracks[genreIndex] = newTrackArray;
+                    break;
+                }
+            }
+        }
     }
 
-    // Get the list of music tracks in the library
     public List<MusicTrack> getTracks() {
-        // return the list of music tracks
-        return tracks;
+        // Get all tracks from the 2D array
+        List<MusicTrack> allTracks = new ArrayList<>();
+        for (int i = 0; i < tracks.length; i++) {
+            for (int j = 0; j < tracks[i].length; j++) {
+                if (tracks[i][j] != null) {
+                    allTracks.add(tracks[i][j]);
+                }
+            }
+        }
+        return allTracks;
     }
 
-    // Get the number of music tracks in the library
+    public String[] getGenres() {
+        // Create an array of all genres
+        String[] genres = new String[tracks.length];
+        for (int i = 0; i < genres.length; i++) {
+            genres[i] = String.format("Genre %d", i + 1);
+        }
+        return genres;
+    }
+
     public int getTrackCount() {
-        // return the number of music tracks in the list
-        return tracks.size();
+        // Get the total number of tracks in the library
+        return getTracks().size();
     }
 
-    // Get a music track from the library by index
     public MusicTrack getTrack(int index) {
-        // return the music track at the specified index in the list
-        return tracks.get(index);
+        // Get the track at the specified index from all genres and albums
+        return getTracks().get(index);
+    }
+
+    public List<MusicTrack> getTracksByGenre(String genre) {
+        // Get all tracks from a specific genre
+        int genreIndex = getGenreIndex(genre);
+        List<MusicTrack> genreTracks = new ArrayList<>();
+        for (int i = 0; i < tracks[genreIndex].length; i++) {
+            if (tracks[genreIndex][i] != null) {
+                genreTracks.add(tracks[genreIndex][i]);
+            }
+        }
+        return genreTracks;
+    }
+
+    public List<MusicTrack> getTracksByAlbum(String album) {
+        // Get all tracks from a specific album
+        List<MusicTrack> albumTracks = new ArrayList<>();
+        for (int i = 0; i < tracks.length; i++) {
+            for (int j = 0; j < tracks[i].length; j++) {
+                if (tracks[i][j] != null && tracks[i][j].getAlbum().equals(album)) {
+                    albumTracks.add(tracks[i][j]);
+                }
+            }
+        }
+        return albumTracks;
+    }
+
+    private int getGenreIndex(String genre) {
+        // Get the index of the genre in the 2D array
+        int genreIndex = -1;
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i][0] != null && tracks[i][0].getGenre().equals(genre)) {
+                genreIndex = i;
+                break;
+            }
+        }
+        return genreIndex;
     }
 }
+
